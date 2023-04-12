cb11e20a62c0cfafbf9abbc9c8a4eda6f6602b10
Dzeko <54543227+lubosjacko@users.noreply.github.com>
2023-04-12 17:17:29 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,6 +12,10 @@ terraform {
   required_version = ">= 1.1.0"
 }
 
+provider "azurerm" {
+  features {}
+}
+
 #-------------------------------
 # Local Declarations
 #-------------------------------
